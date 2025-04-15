c9ef68bf429d4ef14934d48a97ea14d483c1488e
kom1lov001 <avazkomilov94@gmail.com>
2025-04-15 20:35:40 +0500

    first commit

---
+++ src/routers/routers.ts
@@ -0,0 +1,15 @@
+// import { createWebHistory, createRouter } from "vue-router";
+
+// import LandingPageView from "../views/nexcent/LandingView.vue";
+// import back from "../views/back.vue";
+
+// const routes = [
+//   { path: "/", component: LandingPageView },
+//   { path: "/bv", component: back },
+// ];
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// });
+// export default router;
---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.ts
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4CAF4F",
+        secondary: "#9333EA",
+        accent: "#FBBF24",
+        dark: "#1F2937",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
